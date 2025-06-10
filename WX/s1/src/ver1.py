f09246b1c76dacc0bc960877c97f474840ad8f1e
kachraz <110726496+kachraz@users.noreply.github.com>
2025-06-10 06:50:32 +0000

    🫦

--- WX/s1/src/ver1.py
+++ WX/s1/src/ver1.py
@@ -2,15 +2,29 @@
 # s1.py -= version 1 of the work - following tutorial
 # ================================================
 
-from .utz import header1
-import os 
+import os
+
 from dotenv import load_dotenv
+from rich import print as rprint
+
+from .utz import header1
+
+# === Get Env ===
+load_dotenv("src/.env")
+hf_token = os.getenv("HF")
 
 
 # === Main File Entry Funvtion===
 
 
 def ver1_main():
-    header1("Ver1 Main entry")
+    env_print()
 
 # === Sun functions ===
+
+# --- test print function of the env ---
+
+
+def env_print():
+    header1("Env Print")
+    rprint(hf_token)
