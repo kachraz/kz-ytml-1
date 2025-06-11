89c0655dea0180003dd7927175a104b9120327f5
kachraz <110726496+kachraz@users.noreply.github.com>
2025-06-11 17:01:10 +0000

    🫦

---
+++ WX/tezt/groq/g1/buty.py
@@ -0,0 +1,17 @@
+# /////////////////////////////////////////////////////////////////////
+# Main WomanAssSmelling Module - buty.py
+# /////////////////////////////////////////////////////////////////////
+
+# --- Imports ---
+
+from src.utz import header1
+
+# --- App Code ---
+
+
+def DrinkHerPiss():
+    header1("Smell Her Fart")
+
+
+if __name__ == "__main__":
+    DrinkHerPiss()
--- WX/tezt/groq/g1/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from g1!")
-
-
-if __name__ == "__main__":
-    main()
