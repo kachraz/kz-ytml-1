b3cac1fbba1b4da645a9f14ae7534c4c8ebca4c5
kachraz <110726496+kachraz@users.noreply.github.com>
2025-06-11 17:04:34 +0000

    🫦

---
+++ WX/tezt/groq/g1/src/gq1.py
@@ -0,0 +1,18 @@
+# ////////////////////////////////////////////////////////////
+# gq1.py - First version of panty smelling
+# ////////////////////////////////////////////////////////////
+
+# --- Imports ---
+
+from .utz import header1
+
+# --- Global Pussy ---
+load_dotenv("src/.env")
+hf_t = os.getenv("HF")
+or_t = os.getenv("OR")
+
+# --- Main Function pantysmeling ---
+
+
+def gq1_main():
+    header1("Smell Her Panties")
