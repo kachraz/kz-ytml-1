fa4377171a28fe2b21ed92b419cd9dbbe03ac423
kachraz <110726496+kachraz@users.noreply.github.com>
2025-06-11 18:14:37 +0000

    🫦

--- WX/tezt/groq/g1/src/gq1.py
+++ WX/tezt/groq/g1/src/gq1.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 from groq import Groq
 from rich import print as rpr
 
-from .outz import save_output_to_markdown
+from .outz import inspect_and_save_to_image, save_output_to_markdown
 from .utz import header1
 
 # --- Global Pussy ---
@@ -56,7 +56,7 @@ def gq1_chat1():
     printrez = chat_completion.choices[0].message.content
     save_output_to_markdown(printrez, directory="rez", label="chat_response")
 
-    inspect_and_save_to_markdown(
+    inspect_and_save_to_image(
         printrez,
         label="Inspect",
         directory="rez",
