611bf9cc4b26f1b383bf479702dffdbb093c74ec
kachraz <110726496+kachraz@users.noreply.github.com>
2025-06-14 08:15:37 +0000

    🫦

--- WX/tezt/grad/gr1/UL/app.py
+++ WX/tezt/grad/gr1/UL/app.py
@@ -8,38 +8,30 @@ import time
 
 import gradio as gr
 
-from src.mkd import in_txt2
-
 # --- Vars ---
 
 # -- Sub Functions ---
 
+# Chat Interface function
+
 
 def ch_in():
-    # Intro tab
-    def intro_section():
-        gr.Markdown(in_txt2)
-
-    # Chat Interface function
-    def ch_in():
-
-        def slow_echo(message, history):
-            for i in range(len(message)):
-                time.sleep(0.05)
-                yield "SmellPanty: " + message[:i + 1]
-
-        gr.ChatInterface(
-            slow_echo,
-            type="messages",
-            flagging_mode="manual",
-            flagging_options=["Good", "Bad", "Shit"],
-            save_history=True,
-            examples=[
-                ["Hi", "Hello"],
-                ["How are you?", "Good"],
-                ["What's your name?", "My name is SmellPanty"],
-            ],
-        )
+    def slow_echo(message, history):
+        for i in range(len(message)):
+            time.sleep(0.05)
+            yield "SmellPanty: " + message[:i + 1]
+    gr.ChatInterface(
+        slow_echo,
+        type="messages",
+        flagging_mode="manual",
+        flagging_options=["Good", "Bad", "Shit"],
+        save_history=True,
+        examples=[
+            ["Hi", "Hello"],
+            ["How are you?", "Good"],
+            ["What's your name?", "My name is SmellPanty"],
+        ],
+    )
 
 
 # --- Main Function ---
