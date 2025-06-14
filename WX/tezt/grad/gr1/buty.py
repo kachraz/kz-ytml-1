185d0771e731b04711462a49152e2472039561ad
kachraz <110726496+kachraz@users.noreply.github.com>
2025-06-14 07:04:30 +0000

    🫦

--- WX/tezt/grad/gr1/buty.py
+++ WX/tezt/grad/gr1/buty.py
@@ -4,12 +4,13 @@
 
 # --- Imports ---
 
-from src.utz import eline, header1, tline
+from src.g1 import g1_main
+from src.utz import eline, tline
 
 
 # --- Main Function ---
 def buty():
-    header1("Gradio Tests")
+    g1_main()
 
 
 if __name__ == "__main__":
