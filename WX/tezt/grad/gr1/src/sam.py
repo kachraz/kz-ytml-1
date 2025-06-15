5a9ce481549faaaa0a287b6e6c079ee8c34117fb
kachraz <110726496+kachraz@users.noreply.github.com>
2025-06-15 07:06:27 +0000

    🫦

--- WX/tezt/grad/gr1/src/sam.py
+++ WX/tezt/grad/gr1/src/sam.py
@@ -16,12 +16,28 @@ from src.utz import header1
 load_dotenv("src/.ass")
 SA_T = os.getenv("SAO")
 
+themes = [
+    gr.themes.Ocean(),
+    gr.themes.Monochrome(),
+    gr.themes.Citrus(),
+    gr.themes.Glass(),
+    gr.themes.Default(),
+    gr.themes.Soft()
+]
+c_th = themes[5]
+
+# Styling Css
+czz = """ 
+.gradio-container {background: url(https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTJ4b21tZ3hxdmQwYzVuejU1cnZ3dXZkc3hwYnJwdDhmcjlxbWY1MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7b8jdNUoFBdcoILjjv/giphy.gif); background-attachment: fixed; background-repeat: no-repeat; background-size: cover; background-position: center;} 
+"""
+
 # --- Main Function ---
 
 
 def sam_main():
     # ass_print()
-    sam_chat1()
+    # sam_chat1()
+    sam_chat2()
 
 # --- Sub Functions ---
 
@@ -64,5 +80,61 @@ def sam_chat1():
     )
 
     panty.launch(
+        show_error=True
+    )
+
+
+# Same chat as above but now with using blocks
+
+def sam_chat2():
+    header1("Samchat 2 - Blocks Layout")
+
+    intro_txt = """
+# Samnova Chat with 
+1. Tabbed interface 
+2. UI Styling 
+3. BG Fixes
+"""
+
+    def intr_tab():
+        gr.Markdown(intro_txt)
+
+    def chat_func():
+        client = OpenAI(
+            base_url="https://api.sambanova.ai/v1",
+            api_key=SA_T
+        )
+
+        def predict(message, history):
+            history.append({"role": "user", "content": message})
+            stream = client.chat.completions.create(
+                model="Meta-Llama-3.2-1B-Instruct",
+                messages=history,
+                stream=True
+            )
+            chunks = []
+            for chunk in stream:
+                chunks.append(chunk.choices[0].delta.content or "")
+                yield "".join(chunks)
+
+        panty = gr.ChatInterface(
+            predict,
+            title="Samova Chatbot",
+            description="Chat with Samova",
+            type="messages",
+            flagging_mode="manual",
+        )
+
+    with gr.Blocks(
+        theme=c_th,
+    ) as pty:
+
+        with gr.Tab("Intro Tab"):
+            intr_tab()
+
+        with gr.Tab("Chat"):
+            chat_func()
+
+    pty.launch(
         show_error=True
     )
