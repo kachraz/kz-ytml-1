fae006b792e85caedac6933333fcf49c22aeec09
kachraz <110726496+kachraz@users.noreply.github.com>
2025-06-16 07:19:48 +0000

    🫦

---
+++ WX/agentz/sm3/buty.py
@@ -0,0 +1,18 @@
+# /////////////////////////////////////////////////////////////
+# ver1 - Gradio Tests
+# /////////////////////////////////////////////////////////////
+
+# --- Imports ---
+
+from src.utz import eline, header1, tline
+
+
+# --- Main Function ---
+def buty():
+    header1("Gradio Test")
+
+
+if __name__ == "__main__":
+    tline()
+    buty()
+    eline()
