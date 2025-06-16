3fa244b78ee8b4744e881ff51d8a40bd6d5ed540
kachraz <110726496+kachraz@users.noreply.github.com>
2025-06-16 08:16:57 +0000

    🫦

--- WX/tezt/neby/n1/src/nb1.py
+++ WX/tezt/neby/n1/src/nb1.py
@@ -7,6 +7,7 @@
 import os
 
 from dotenv import load_dotenv
+from openai import OpenAI
 from rich import print as rpr
 
 from .utz import header1
@@ -34,3 +35,16 @@ def brint_env():
 
 def nb_test1():
     header1("NB1 Tezt")
+
+    client = OpenAI(
+        base_url="https://api.sambanova.ai/v1",
+        api_key=NB_T
+    )
+
+    completion = client.chat.completions.create(
+        model="Meta-Llama-3.2-1B-Instruct",
+        messages=[
+            {"role": "user", "content": "Describe booty dancing"}
+        ],
+        temperature=0.5
+    )
